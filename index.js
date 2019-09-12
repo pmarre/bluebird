d543608a75fa9e99a8bcdc501470ec68b3117743
Patrick Marre <pmarre40@gmail.com>
2019-09-12 07:41:30 -0700

    Animations

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-let hiddenClass = document.querySelectorAll(".js--intro");
-let introContainer = document.querySelector(".intro__container");
-// let headerImage = document.querySelector('.')
+const hiddenClass = document.querySelectorAll('.js--intro');
+const introContainer = document.querySelector('.intro__container');
+const navOverlay = document.querySelector('.nav__overlay');
+const navMenuBtn = document.querySelector('.nav__icon');
+const closeBtn = document.querySelector('.nav--close_btn');
+const navList = document.querySelector('.nav--mobile_list');
 
 function removeIntroHidden() {
   for (let i = 0; i < hiddenClass.length; i++) {
-    hiddenClass[i].classList.remove("intro--hidden");
+    hiddenClass[i].classList.remove('intro--hidden');
   }
-  introContainer.style.display = "none";
+  introContainer.style.display = 'none';
 }
 
 function showSection() {
-  document.querySelector(".work").classList.add("animate");
+  document.querySelector('.work').classList.add('animate');
 }
 
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
   setTimeout(removeIntroHidden, 7000);
   setTimeout(showSection, 2000);
 });
+
+function showNavigation() {
+  navOverlay.classList.remove('hide--nav');
+  navOverlay.classList.add('show--nav');
+  navList.classList.add('nav--mobile_list--show');
+  closeBtn.classList.add('nav--close_btn--show');
+}
+
+function hideNavigation() {
+  navOverlay.classList.remove('show--nav');
+  navList.classList.remove('nav--mobile_list--show');
+  closeBtn.classList.remove('nav--close_btn--show');
+  navOverlay.classList.add('hide--nav');
+}
+
+navMenuBtn.addEventListener('click', showNavigation);
+closeBtn.addEventListener('click', hideNavigation);
